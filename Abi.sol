1fb260d4ccbc5594f387d3e4d9cf522dad3b3f6f
wimgithub <934032173@qq.com>
2021-04-19 15:29:55 +0800

    update sol

---
+++ Abi.sol
@@ -0,0 +1,10 @@
+
+contract ABI {
+    function add_encode(address strategy,address _token0,address _token1,uint256 _token0Amount,uint256 _token1Amount,uint256 _minLPAmount) pure public returns(bytes memory) {
+        return(abi.encode(strategy,abi.encode(_token0,_token1,_token0Amount,_token1Amount,_minLPAmount)));
+    }
+    
+    function withdraw_encode(address strategy,address _token0,address _token1,uint _whichWantBack)pure public returns(bytes memory){
+        return (abi.encode(strategy,abi.encode(_token0,_token1,_whichWantBack)));
+    }
+}
